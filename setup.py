0ad56b557c62df94e7461b0aa06b410c8b493715
Murat Guchetl <gmurka@gmail.com>
2018-03-27 12:21:35 +0300

    fixed setup.py errors

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='.'.join(map(str, __import__('shopkit').VERSION)),
+    version='.'.join(map(str, __import__('diversity').VERSION)),
 
     description=('A set of extensions for django-diverse project'),
     long_description=long_description,
